931d06f9f9574a45e3500dc46e54443afb7a02b4
Aleenwaleed <aleen_17@outlook.com>
2025-08-11 21:39:20 +0300

    Update 6 11/8

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,9 +1,11 @@
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from './components/Nav';
 import CallToAction from './components/CallToAction'
 import Specials from './components/specials';
 import Footer from './components/Footer';
 import CustomersSay from './components/CustomersSay'
 import Chicago from './components/Chicago'
+import BookingPage from "./components/BookingPage";
 import './App.css';
 
 
@@ -16,6 +18,7 @@ function App() {
       <CustomersSay />
       <Chicago />
       <Footer />
+      <BookingPage />
     </>
   );
 }
---
+++ little-lemon/src/components/BookingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BookingForm from "./BookingForm";
+
+export default function BookingPage() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Reserve a Table</h1>
+      <BookingForm />
+    </main>
+  );
+}
